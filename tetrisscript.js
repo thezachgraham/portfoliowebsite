e98e9d873bc2f763ff007c8597e36cb79bb3acde
zachgraham20 <83549086+zachgraham20@users.noreply.github.com>
2021-05-06 14:05:22 -0400

    Add files via upload

---
+++ tetrisscript.js
@@ -0,0 +1,10 @@
+const lTetromino = 'firstShape'
+const zTetromino = 'secondShape'
+const oTetromino = 'thirdShape'
+const iTetromino = 'fourthShape'
+const tTetromino = 'fifthShape'
+
+
+const tetrominoes = [lTetromino, zTetromino, oTetromino, iTetromino, tTetromino]
+
+console.log(tetrominoes[5])
